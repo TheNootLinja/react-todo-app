022265b1b1507cab3a43d4237595dbbe7e231aba
TheNootLinja <thenootlinja@gmail.com>
2023-07-03 13:01:33 -0400

    Update README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="app-header-container">
+        <h1>ToDo List</h1>
+        <p className='new-task-icon' onClick={() => alert('Popup for adding task')}>+</p>
+      </div>
+      <ul className='todo-card-list'>
+        <li className='todo-card'>
+          <h2>Item #1</h2>
+          <p>This is the description of item #1</p>
+          <p>12/31/2023</p>
+        </li>
+        <li className='todo-card'>
+          <h2>Item #2</h2>
+          <p>This is the description of item #2</p>
+          <p>12/31/2023</p>
+        </li>
+        <li className='todo-card'>
+          <h2>Item #3</h2>
+          <p>This is the description of item #3</p>
+          <p>12/31/2023</p>
+        </li>
+        <li className='todo-card'>
+          <h2>Item #4</h2>
+          <p>This is the description of item #4</p>
+          <p>12/31/2023</p>
+        </li>
+      </ul>
     </div>
   );
 }
