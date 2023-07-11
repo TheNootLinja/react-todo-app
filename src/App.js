52c2a9488ace6c2dd0fb0bd7c40daeebf978a325
TheNootLinja <thenootlinja@gmail.com>
2023-07-11 16:53:44 -0400

    Add local storage functionality, ability to remove tasks, and scaffold out ability to add new tasks

--- src/App.js
+++ src/App.js
@@ -1,33 +1,87 @@
 import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
+  // Lockout state
+  const [lockoutVisible, setLockoutVisible] = useState(false);
+  // Test data: array of tasks
+  const testDataArr = [
+    {
+      taskTitle: "Task #1",
+      taskDesc: "Description of task #1",
+      taskDate: "12/31/2023",
+      taskId: "1"
+    },
+    {
+      taskTitle: "Task #2",
+      taskDesc: "Description of task #2",
+      taskDate: "12/31/2023",
+      taskId: "2"
+    },
+    {
+      taskTitle: "Task #3",
+      taskDesc: "Description of task #3",
+      taskDate: "12/31/2023",
+      taskId: "3"
+    },
+  ];
+  // Task array state
+  const [taskArr, setTaskArr] = useState([]);
+  function changeLockoutVisible() {
+    setLockoutVisible(!lockoutVisible)
+  }
+
+  // TODO: Write function for getting existing tasks from local storage on app load
+  useEffect(() => {
+    localStorage.getItem("existingTaskArr") ? setTaskArr(JSON.parse(localStorage.getItem("existingTaskArr"))) : setTaskArr([])
+  },[])
+
+  function addTask() {
+    const newTask = {
+        taskTitle: "Task #1",
+        taskDesc: "Description of task #1",
+        taskDate: "12/31/2023",
+        taskId: Math.floor(Math.random() * 100)
+    };
+    const newAddTaskArr = [...taskArr, newTask]
+    setTaskArr(newAddTaskArr);
+    localStorage.setItem("existingTaskArr", JSON.stringify(newAddTaskArr))
+  }
+
+  // Function for removing a task
+  function removeTask(id) {
+    // Update task array state
+    const newRemTaskArr = taskArr.filter((task) => task.taskId != id);
+    setTaskArr(newRemTaskArr);
+    // Update the local storage array of tasks
+    localStorage.setItem("existingTaskArr", JSON.stringify(newRemTaskArr));
+  }
+
+  // TODO: Write function for creating a new task
+  
   return (
     <div className="App">
+      {lockoutVisible ? (
+        <div className="lockout">
+          <button className="close-lockout-btn" onClick={addTask}>New Task</button>
+          <button className="close-lockout-btn" onClick={changeLockoutVisible}>Close</button>
+        </div>
+      ) : (
+        false
+      )}
       <div className="app-header-container">
         <h1>ToDo List</h1>
-        <p className='new-task-icon' onClick={() => alert('Popup for adding task')}>+</p>
+        <p className='new-task-icon' onClick={changeLockoutVisible}>{lockoutVisible?"x":"+"}</p>
       </div>
       <ul className='todo-card-list'>
-        <li className='todo-card'>
-          <h2>Item #1</h2>
-          <p>This is the description of item #1</p>
-          <p>12/31/2023</p>
-        </li>
-        <li className='todo-card'>
-          <h2>Item #2</h2>
-          <p>This is the description of item #2</p>
-          <p>12/31/2023</p>
-        </li>
-        <li className='todo-card'>
-          <h2>Item #3</h2>
-          <p>This is the description of item #3</p>
-          <p>12/31/2023</p>
-        </li>
-        <li className='todo-card'>
-          <h2>Item #4</h2>
-          <p>This is the description of item #4</p>
-          <p>12/31/2023</p>
-        </li>
+        {taskArr.map((task, index) => 
+          <li className='todo-card' key={task.taskId}>
+            <h1>{task.taskTitle}</h1>
+            <p>{task.taskDesc}</p>
+            <p>{task.taskDate}</p>
+            <button onClick={ () => removeTask(task.taskId)}>Delete</button>
+          </li>
+        )}
       </ul>
     </div>
   );
